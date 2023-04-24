0c78d0a769e9ff92b0c5bc50fb12f884bca16493
thiago <tmg3.mn@gmail.com>
2023-04-24 16:34:35 -0300

    init

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+const defaultTheme = {
+  colors: {
+    purple: "#191826",
+    white: "#fcfaf9",
+    gold: "#ffb964",
+    orange: '#fa7453',
+    blue: '#66e2dc'
+  },
+  typography: {
+    ibmB1: '700 5.5rem/88px "IBM Plex Sans", sans-serif',
+    ibmB2: '700 2.5rem/52px "IBM Plex Sans", sans-serif',
+    ibmR1: '400 1.25rem/34px "IBM Plex Sans", sans-serif',
+    ibmR2: '400 1rem/26px "IBM Plex Sans", sans-serif'
+  },
+}
