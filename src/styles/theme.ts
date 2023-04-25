797a34db46fc7115d146bf71e033ced4988ecd76
thiago <tmg3.mn@gmail.com>
2023-04-25 14:47:31 -0300

    Update Theme styles

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -9,7 +9,7 @@ const defaultTheme = {
   typography: {
     ibmB1: '700 5.5rem/88px "IBM Plex Sans", sans-serif',
     ibmB2: '700 4rem/64px "IBM Plex Sans", sans-serif',
-    ibmB3: '700 2.5rem/52px "IBM Plex Sans", sans-serif',
+    ibmB3: '700 2.5rem/48px "IBM Plex Sans", sans-serif',
     ibmR1: '400 1.25rem/34px "IBM Plex Sans", sans-serif',
     ibmR2: '400 1.125rem/28px "IBM Plex Sans", sans-serif',
     ibmR3: '400 1rem/26px "IBM Plex Sans", sans-serif'
