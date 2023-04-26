1c6d54275ee739a77d2e94c225cd1d3656ba6f9d
thiago <tmg3.mn@gmail.com>
2023-04-26 17:01:00 -0300

    Add mobile responsiveness for 'Footer' component

--- src/components/Footer/styles.tsx
+++ src/components/Footer/styles.tsx
@@ -10,6 +10,12 @@ export const FooterContainer = styled.div`
   align-content: flex-start;
   padding: 2rem 0 5rem;
 
+  ${Media.mobile} {
+    flex-direction: column;
+    gap: 2.5rem;
+    padding: 0 0 3rem 1rem;
+  }
+
   .rights-content {
     display: flex;
     width: 55%;
@@ -20,17 +26,31 @@ export const FooterContainer = styled.div`
       gap: 1.5rem;
     }
 
+    ${Media.mobile} {
+      flex-direction: column;
+      gap: 1.5rem;
+      width: 100%;
+    }
+
     .logo {
       height: 30%;
 
       ${Media.tablet} {
         width: 30%;
       }
+
+      ${Media.mobile} {
+        width: 50%;
+      }
     }
 
     .rights {
       font: ${Theme.typography.ibmR3};
       color: ${Theme.colors.purple};
+
+      ${Media.mobile} {
+        width: 100%;
+      }
 
       span {
         font: 700 1rem/26px 'IBM Ples Sans', sans-serif;
@@ -42,6 +62,10 @@ export const FooterContainer = styled.div`
     display: flex;
     align-self: center;
     gap: 1rem;
+
+    ${Media.mobile} {
+      align-self: flex-start;
+    }
 
     .icon {
       &:hover {
