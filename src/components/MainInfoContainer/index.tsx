6685fe8b38d295c167b2fdb16eb0a6e46cb2a305
thiago <tmg3.mn@gmail.com>
2023-04-27 11:04:04 -0300

    Fix structure for MainInfoContainer

--- src/components/MainInfoContainer/index.tsx
+++ src/components/MainInfoContainer/index.tsx
@@ -6,18 +6,18 @@ const index = () => {
   return (
     <InfoContainer>
       <h2 className="info-h2">Premium EQ</h2>
-        <p className="info-p">Get expert-level control with a robust equalizer, volume mixer, and spatial audio. Take your listening experience to a whole new level and access all our incredible features!</p>
-        <h1 className="info-h1">$4 <span> /month</span></h1>
-        <div className="btns-container">
-          <button className="apple-btn btn">
-            <img src={AppleLogo} alt="" />
-            iOS Download
-          </button>
-          <button className="android-btn btn">
-            <img src={AndroidLogo} alt="" />
-            Android Download
-          </button>
-        </div>
+      <p className="info-p">Get expert-level control with a robust equalizer, volume mixer, and spatial audio. Take your listening experience to a whole new level and access all our incredible features!</p>
+      <h1 className="info-h1">$4 <span> /month</span></h1>
+      <div className="btns-container">
+        <button className="apple-btn btn">
+          <img src={AppleLogo} alt="" />
+          iOS Download
+        </button>
+        <button className="android-btn btn">
+          <img src={AndroidLogo} alt="" />
+          Android Download
+        </button>
+      </div>
     </InfoContainer>
   )
 }
