1a4d72ec940a023e0ec73a147974e28c9147532b
thiago <tmg3.mn@gmail.com>
2023-05-01 09:33:22 -0300

    Adjust indentation of MainInfoContainer styles

--- src/components/MainInfoContainer/styles.tsx
+++ src/components/MainInfoContainer/styles.tsx
@@ -3,46 +3,46 @@ import Theme from '../../styles/theme';
 import Media from '../../styles/media';
 
 export const InfoContainer = styled.div`
-  padding: 2.75rem 2.75rem 2rem;
-    background-color: ${Theme.colors.orange};
-    border-radius: 12px;
-    color: ${Theme.colors.white};
-    width: 446px;
-    height: 625px;
+  padding: 2.75rem;
+  background-color: ${Theme.colors.orange};
+  border-radius: 12px;
+  color: ${Theme.colors.white};
+  width: 446px;
+  height: 625px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  position: absolute;
+  bottom: -100px;
+  right: 110px;
+  z-index: 2;
+
+  ${Media.mobile} {
+    width: 100%;
+    position: initial;
+    gap: 1.25rem;
+  }
+
+  .info-h2 {
+    font: ${Theme.typography.ibmB3};
+    line-height: 52px;
+  }
+
+  .info-p {
+    font: ${Theme.typography.ibmR1};
+    line-height: 34px;
+  }
+
+  .info-h1 {
+    font: ${Theme.typography.ibmB2};
+    line-height: 52px;
     display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    position: absolute;
-    bottom: -100px;
-    right: 110px;
-    z-index: 2;
+    align-items: center;
+    gap: 1rem;
 
-    ${Media.mobile} {
-      width: 100%;
-      position: initial;
-      gap: 1.25rem;
-    }
-
-    .info-h2 {
-      font: ${Theme.typography.ibmB3};
-      line-height: 52px;
-    }
-
-    .info-p {
+    span {
       font: ${Theme.typography.ibmR1};
-      line-height: 34px;
+      line-height: 32px;
     }
-
-    .info-h1 {
-      font: ${Theme.typography.ibmB2};
-      line-height: 52px;
-      display: flex;
-      align-items: center;
-      gap: 1rem;
-
-      span {
-        font: ${Theme.typography.ibmR1};
-        line-height: 32px;
-      }
-    }
+  }
 `
